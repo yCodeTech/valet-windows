d0d64940c7584b1137f8feaf0ee2c5d8358f795c
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2020-12-26 21:39:25 +0200

    Add Windows integration tests

---
+++ tests/WindowsIntegrationTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests;
+
+use Tests\Support\TestProcess;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Process\Process;
+
+/**
+ * @group integration
+ */
+class WindowsIntegrationTest extends TestCase
+{
+    /** @test */
+    public function install_valet()
+    {
+        $this->exec('valet install')
+            ->assertSuccessful()
+            ->assertContains('Valet installed successfully');
+
+        $this->exec('Get-Service -Name "valet_nginx"')
+            ->assertSuccessful()
+            ->assertContains('Running  valet_nginx');
+
+        $this->exec('Get-Service -Name "valet_phpcgi"')
+            ->assertSuccessful()
+            ->assertContains('Running  valet_phpcgi');
+
+        $this->exec('Get-Service -Name "AcrylicDNSProxySvc"')
+            ->assertSuccessful()
+            ->assertContains('Running  AcrylicDNSProxySvc');
+
+        $this->exec('curl.exe --max-time 5 http://localhost')
+            ->assertSuccessful()
+            ->assertContains('<title>Valet - Not Found</title>');
+
+        $this->exec('valet')
+            ->assertSuccessful();
+
+        $this->exec('ls ~/.config/valet')
+            ->assertContains('Certificates')
+            ->assertContains('Drivers')
+            ->assertContains('Extensions')
+            ->assertContains('Log')
+            ->assertContains('Nginx')
+            ->assertContains('Sites')
+            ->assertContains('AcrylicHosts.txt')
+            ->assertContains('config.json');
+    }
+
+    /**
+     * @param  string|array $command
+     * @return \Tests\Support\TestProcess
+     */
+    protected function exec($command): TestProcess
+    {
+        if (is_array($command)) {
+            $command = implode(';', $command);
+        }
+
+        $process = Process::fromShellCommandline("powershell -Command \"{$command}\"");
+        $process->setTimeout(30)->run();
+
+        return new TestProcess($process);
+    }
+}
