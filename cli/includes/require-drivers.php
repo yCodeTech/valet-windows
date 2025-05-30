8a94888825ecaf86aa7f3a888415bf4f8b5c72c6
yCodeTech <studocwho@hotmail.com>
2025-05-30 02:35:00 +0100

    fix: `PHP Fatal error:  Uncaught Error: Class "Specific/{driver}" not found` error.
    When using the deprecated legacy drivers, a PHP fatal error occurs for all legacy drivers, saying that the `Specific` respective driver class that the legacy driver extends can not be found. Extending the namespaced class is supposed to prevent this using the PSR-4 autoloading, but autoloading doesn't seem to work here.
    
    To fix, we need to `require` all of the `Specific` namespaced drivers before the legacy drivers are `require`d. This then ensures all namespaced classes are available in the legacy drivers, and fixes the PHP class not found error.

--- cli/includes/require-drivers.php
+++ cli/includes/require-drivers.php
@@ -5,8 +5,6 @@ $includesDir = __DIR__;
 $valetDriversDir = "$includesDir/../Valet/Drivers";
 
 require_once "$valetDriversDir/ValetDriver.php";
-require_once "$includesDir/legacy_drivers/ValetDriver.php";
-
 
 foreach (scandir($valetDriversDir) as $file) {
 	$path = "$valetDriversDir/$file";
@@ -15,9 +13,32 @@ foreach (scandir($valetDriversDir) as $file) {
 	}
 }
 
-// Require legacy drivers.
+/**
+ * Require legacy drivers
+ */
+
 // DEPRECATED: The legacy drivers are deprecated as of v3.2.0 and therefore so is this code.
-$legacyDriversDir = "$includesDir/legacy_drivers";
+
+// Require specific drivers.
+
+// For the legacy drivers to work, we need to require the specific drivers first.
+// This is because the legacy drivers extend the specific drivers.
+// When the legacy drivers are removed in 4.0.0, this will no longer be necessary,
+// as the specific drivers will just work via PSR-4 autoloading.
+
+require_once "$includesDir/legacy_drivers/ValetDriver.php";
+
+$specificDriversDir = "$valetDriversDir/Specific";
+
+foreach (scandir($specificDriversDir) as $file) {
+	$path = "$specificDriversDir/$file";
+	if (substr($file, 0, 1) !== '.' && ! is_dir($path)) {
+		require_once $path;
+	}
+}
+
+// Require legacy drivers.
+$legacyDriversDir = "$includesDir/legacy";
 
 foreach (scandir($legacyDriversDir) as $file) {
 	if (substr($file, 0, 1) !== '.') {
